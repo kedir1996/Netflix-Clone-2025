e685707b52d2293d8dc6a7c8a2d28942d3cb703a
kedir1996 <kedirahmed19963384@gmail.com>
2025-06-05 08:06:48 +0100

    Header and Footer

---
+++ Netflix-clone/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import Header from './Header/header.jsx';
+import './Header/header.css';
+import Footer from './Footer/footer.jsx';
+import './Footer/footer.css';
+
+// Import components
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Netflix-clone/src/Header/header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Netflixlogo from "../assets/image/Netflix.logo.jpg";
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+function Header() {
+  return (
+    <div className="Header-Outer-container">
+      <div className="Header-container">
+        <div className="Header-left">
+          <ul>
+            <li>
+              <img src={Netflixlogo} alt="Netflixlogo" width="100" />
+            </li>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse BY Language</li>
+          </ul>
+        </div>
+        <div className="Header-right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
